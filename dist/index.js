fb7827248be4d5cd9ce5cf0bc3780ae63554d1b0
ZhouTian0716 <egglord0716@gmail.com>
2022-09-19 21:06:14 +1000

    regular commit

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,60 @@
 "use strict";
+// Basic Types
+console.log("hi");
 let id = 5;
-id = 50000;
-console.log("ID:", id);
+let company = "Traversy Media";
+let isPublished = true;
+let x = "Hello";
+let ids = [1, 2, 3, 4, 5];
+let arr = [1, true, "string"];
+// Tuple
+let person = [1, "Brad", true];
+// Tuple Array
+let employee;
+employee = [
+    [1, "Brad"],
+    [2, "John"],
+    [3, "Jill"],
+];
+// Union
+let productId = 22;
+productId = 23;
+productId = "23";
+// Enum
+var Direction1;
+(function (Direction1) {
+    Direction1[Direction1["Up"] = 0] = "Up";
+    Direction1[Direction1["Down"] = 1] = "Down";
+    Direction1[Direction1["Left"] = 2] = "Left";
+    Direction1[Direction1["Right"] = 3] = "Right";
+})(Direction1 || (Direction1 = {}));
+var Direction2;
+(function (Direction2) {
+    Direction2["Up"] = "Up";
+    Direction2["Down"] = "Down";
+    Direction2["Left"] = "Left";
+    Direction2["Right"] = "Right";
+})(Direction2 || (Direction2 = {}));
+console.log(Direction2.Up);
+const user = {
+    id: 1,
+    name: "John",
+};
+// Type Assertion
+let cid = 1;
+// let customerId=<number>cid
+let customerId = cid;
+//Functions
+function addNum(x, y) {
+    return x + y;
+}
+console.log(addNum(1, 2));
+// Void
+function loggingOut(message) {
+    console.log(message);
+}
+loggingOut("true");
+const user1 = {
+    id: 2,
+    name: "Brad",
+};
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,81 @@
+// Basic Types
+console.log("hi");
 let id: number = 5;
-id = 50000;
-console.log("ID:",id);
+let company: string = "Traversy Media";
+let isPublished: boolean = true;
+let x: any = "Hello";
+
+let ids: number[] = [1, 2, 3, 4, 5];
+let arr: any[] = [1, true, "string"];
+
+// Tuple
+let person: [number, string, boolean] = [1, "Brad", true];
+// Tuple Array
+let employee: [number, string][];
+
+employee = [
+  [1, "Brad"],
+  [2, "John"],
+  [3, "Jill"],
+];
+
+// Union
+let productId: string | number = 22;
+productId = 23;
+productId = "23";
+
+// Enum
+enum Direction1 {
+  Up,
+  Down,
+  Left,
+  Right,
+}
+
+enum Direction2 {
+  Up = "Up",
+  Down = "Down",
+  Left = "Left",
+  Right = "Right",
+}
+
+console.log(Direction2.Up);
+
+// Objects
+type User = { id: number; name: string };
+
+const user: User = {
+  id: 1,
+  name: "John",
+};
+
+// Type Assertion
+let cid: any = 1;
+// let customerId=<number>cid
+let customerId = cid as number;
+
+//Functions
+function addNum(x: number, y: number): number {
+  return x + y;
+}
+
+console.log(addNum(1, 2));
+
+// Void
+function loggingOut(message: string | number): void {
+  console.log(message);
+}
+loggingOut("true");
+
+// Interfaces
+interface UserInterface {
+  id: number;
+  name: string;
+  age?:number
+}
+
+const user1: UserInterface = {
+  id: 2,
+  name: "Brad",
+};
+
